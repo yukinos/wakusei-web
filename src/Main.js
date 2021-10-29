f0c518caf1d092f04a5dd3330ff2284f55a09f6e
yukinoshita <pcytm8@gmail.com>
2021-10-30 00:31:14 +0900

    メインを大幅に追加し、Q&Aコーナーを設けました

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Apple from './images/apple-logo.svg';
+import Google from './images/google-play.png';
 
 class Main extends React.Component{
     render(){
@@ -9,12 +11,20 @@ class Main extends React.Component{
                     <p class="text">作業のわくせいは作業通話を専門としたdiscordサーバーです</p>
                     <p class="text">わくせいの調査員としてのんびり作業しませんか？</p>
                     <p class="text">宇宙人があなた作業をサポートしてくれます</p>
-                    <button type="button" onclick="location.href='https://discord.gg/2GgS2JqS'">このサーバーに参加する</button>
-                    <h1>discordとは？</h1>
-                    <p>discordは通話がとても快適にできるすごいSNSなのです</p>
+                    <button class="button1" onclick="location.href='https://discord.gg/2GgS2JqS'">このサーバーに参加する</button>
+                    <h1 class="ques1">Q , discordとは？</h1>
+                    <h2 class="ans1">A , 通話がとても快適にできるすごいSNSなのです</h2>
                     <p>すごいんだあああああああああああ</p>
                     <p>うわああああああああああああああああ</p>
                     <p>僕の話を聞いてよおおおおおおおおおおおお</p>
+                    <h3>discordをダウンロード！↓↓↓</h3>
+                    <img  class ="apple" src={Apple} alt="apple"/>
+                    <img  class="google" src={Google} alt="google"/>
+                    <button class="button2" onclick="location.href='https://discord.com/'">PCの方はこちらから！</button>
+                    <h1 class="ques2">Q , discordサーバーとは？</h1>
+                    <h2 class="ans2">A , よくあるSNSの中で作られるグループの様なものです</h2>
+                    <p>そういうことだってばさ！</p>
+                    
                 </div>
 
             </div>
