bcfa55bfc6f15f3d849382dd4ba4f0eaedce68bb
yukinoshita <pcytm8@gmail.com>
2021-10-29 21:19:41 +0900

    背景を固定してスクロール仕様にしました

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ class Footer extends React.Component{
     render(){
         return(
             <div>
-                <div class="footer">HOMEに戻る</div>
+                <div class="footer">TOPに戻る</div>
             </div>
         );
     }
--- src/Main.js
+++ src/Main.js
@@ -4,13 +4,19 @@ class Main extends React.Component{
     render(){
         return(
             <div>
-                <div class="back">
+                <div class="back-ground">
                     <h1 class="title">作業のわくせい</h1>
-                    <p class="text">あなたは深宇宙の探索を行う調査員です</p>
+                    <p class="text">作業のわくせいは作業通話を専門としたdiscordサーバーです</p>
                     <p class="text">わくせいの調査員としてのんびり作業しませんか？</p>
-                    <p class="text">宇宙人が作業をサポートしてくれます</p>
+                    <p class="text">宇宙人があなた作業をサポートしてくれます</p>
                     <button type="button" onclick="location.href='https://discord.gg/2GgS2JqS'">このサーバーに参加する</button>
+                    <h1>discordとは？</h1>
+                    <p>discordは通話がとても快適にできるすごいSNSなのです</p>
+                    <p>すごいんだあああああああああああ</p>
+                    <p>うわああああああああああああああああ</p>
+                    <p>僕の話を聞いてよおおおおおおおおおおおお</p>
                 </div>
+
             </div>
         );
     }
